639a6e37c90f0b77c055a5f0df143d6146f8875b
Mathias Buus <mathiasbuus@gmail.com>
2014-07-03 10:45:25 +0200

    remove old code

--- index.js
+++ index.js
@@ -14,15 +14,6 @@ module.exports = function(writable, readable) {
   }
 
   readable.on('readable', consume)
-  eos(readable, {readable:true, writable:false}, function(err) {
-    error = err || new Error('readable stream closed')
-    consume()
-  })
-
-  eos(writable, {readable:false, writable:false}, function(err) {
-    error = err || new Error('writable stream closed')
-    consume()
-  })
 
   eos(readable, {readable:true, writable:false}, function(err) {
     readableError = readableError || err || new Error('readable stream closed')
